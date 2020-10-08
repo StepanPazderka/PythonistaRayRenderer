40a4a458c5d3fb28d6944007f038b85d61200445
Stepan Pazderka <stepan.pazderka@icloud.com>
2020-10-08 23:40:47 +0200

    Changed res and size of sphere

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ import canvas
 
 def main():
 	console.clear()
-	WIDTH = 400
-	HEIGHT = 250
+	WIDTH = 200
+	HEIGHT = 150
 	camera = Vector(0,0,-1)
-	objects = [Sphere(Point(0,0.5,1.5), 0.5, Color.from_hex("#224F00"))]
+	objects = [Sphere(Point(0,0,1.0), 0.5, Color.from_hex("#F24F00"))]
 	scene = Scene(camera, objects, WIDTH, HEIGHT)
 	engine = RenderEngine()
 	image = engine.render(scene)
